c42253ff96c7c5e52493474a4ec067e44f0aa044
JulioEmanuelGomes <julioemanuel172@gmail.com>
2024-09-23 19:22:31 -0300

    Meu Primeiro Projeto

---
+++ Projetos/TimeOfDay/Time.js
@@ -0,0 +1,26 @@
+const msg = document.getElementById("msg");
+const img = document.getElementById("image");
+const date = new Date()
+const hour = date.getHours()
+
+function carregar() {
+
+
+    if (hour >= 0 && hour < 12) {
+        //BOM DIA
+        img.src = "/img/manha.jpg";
+        document.body.style.background = "#e2cd9f"
+        msg.innerHTML = `Agora São ${hour}Hrs da Manhã`
+
+    } else if (hour >= 12 && hour < 18) {
+        //BOA TARDE
+        img.src = "/img/tarde.jpg";
+        document.body.style.background = "#b9846f"
+        msg.innerHTML = `Agora São ${hour}hrs da Tarde`
+    } else {
+        //BOA NOITE
+        img.src = "/img/noite.jpg";
+        document.body.style.background = "#515154"
+        msg.innerHTML = `Agora São ${hour}hrs da Noite`
+    }
+}
