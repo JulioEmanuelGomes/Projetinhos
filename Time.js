8a62c879a1e049c51e1342e1c9ad276e40c99417
JulioEmanuelGomes <julioemanuel172@gmail.com>
2024-09-25 17:55:04 -0300

    Finalizei projeto timeofday

--- Time.js
+++ Time.js
@@ -1,4 +1,5 @@
 const msg = document.getElementById("msg");
+const msg2 = document.getElementById("msg2");
 const img = document.getElementById("image");
 const date = new Date()
 const hour = date.getHours()
@@ -11,16 +12,20 @@ function carregar() {
         img.src = "img/manha.jpg";
         document.body.style.background = "#e2cd9f"
         msg.innerHTML = `Agora São ${hour}Hrs da Manhã`
+        msg.innerHTML = `Tenha um Bom dia!! ❤❤`
 
     } else if (hour >= 12 && hour < 18) {
         //BOA TARDE
         img.src = "img/tarde.jpg";
         document.body.style.background = "#b9846f"
         msg.innerHTML = `Agora São ${hour}hrs da Tarde`
+        msg2.innerHTML = `Tenha uma Boa tarde!! ❤❤`
     } else {
         //BOA NOITE
         img.src = "img/noite.jpg";
         document.body.style.background = "#515154"
         msg.innerHTML = `Agora São ${hour}hrs da Noite`
+        msg2.innerHTML = `Tenha uma Boa Noite!! ❤❤`
     }
+
 }
