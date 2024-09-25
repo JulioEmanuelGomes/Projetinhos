9fbe159b643fbc964c4e8da3e7f2e3a38fa144b6
JulioEmanuelGomes <julioemanuel172@gmail.com>
2024-09-25 17:38:03 -0300

    fazendo alteração nas imgagens

--- Time.js
+++ Time.js
@@ -8,18 +8,18 @@ function carregar() {
 
     if (hour >= 0 && hour < 12) {
         //BOM DIA
-        img.src = "/img/manha.jpg";
+        img.src = "img/manha.jpg";
         document.body.style.background = "#e2cd9f"
         msg.innerHTML = `Agora São ${hour}Hrs da Manhã`
 
     } else if (hour >= 12 && hour < 18) {
         //BOA TARDE
-        img.src = "/img/tarde.jpg";
+        img.src = "img/tarde.jpg";
         document.body.style.background = "#b9846f"
         msg.innerHTML = `Agora São ${hour}hrs da Tarde`
     } else {
         //BOA NOITE
-        img.src = "/img/noite.jpg";
+        img.src = "img/noite.jpg";
         document.body.style.background = "#515154"
         msg.innerHTML = `Agora São ${hour}hrs da Noite`
     }
